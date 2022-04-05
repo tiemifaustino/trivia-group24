6cccb1ecb4374d40cb392f7e1aceaafd148e3ccb
Laís Nametala Silva <84039617+lalanametala@users.noreply.github.com>
2022-04-05 19:45:53 -0300

    Update src/App.js

--- src/App.js
+++ src/App.js
@@ -7,15 +7,6 @@ import Config from './pages/Config';
 
 export default function App() {
   return (
-    // <>
-    //   <div className="App">
-    //     <header className="App-header">
-    //       <img src={ logo } className="App-logo" alt="logo" />
-    //       <p>
-    //         SUA  VEZ
-    //       </p>
-    //     </header>
-    //   </div>
     <Switch>
       <Route exact path="/game" component={ Game } />
       <Route exact path="/config" component={ Config } />
