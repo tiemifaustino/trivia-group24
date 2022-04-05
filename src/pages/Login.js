f7d330a3374b97d08e74e4ccded252dce45ea7f9
Laís Nametala Silva <84039617+lalanametala@users.noreply.github.com>
2022-04-05 19:45:43 -0300

    Update src/pages/Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import getToken from '../services/fetchToken';
-import saveToken from '../redux/actions';
+import { saveToken } from '../redux/actions';
 
 class Login extends Component {
   state = {
