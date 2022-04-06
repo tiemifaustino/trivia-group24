6892ce972aca05840f0793c6f642bcba42e4c629
Lalá Nametala <lalanametala@gmail.com>
2022-04-06 19:26:34 -0300

    Requisito 17

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,13 +1,32 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
 class Ranking extends Component {
+  handlePlayAgain = () => {
+    const { history } = this.props;
+    history.push('/');
+  };
+
   render() {
     return (
-      <h1 data-testid="ranking-title">
-        Ranking
-      </h1>
+      <>
+        <h1 data-testid="ranking-title">
+          Ranking
+        </h1>
+        <button
+          type="button"
+          data-testid="btn-go-home"
+          onClick={ this.handlePlayAgain }
+        >
+          Play Again
+        </button>
+      </>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.shape(),
+}.isRequired;
 
 export default Ranking;
