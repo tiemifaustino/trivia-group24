be07fb631e7f981eb81dbe635e57d55b3147592b
Tiemi Faustino <hayashipharma@gmail.com>
2022-04-06 16:30:20 -0300

    Requisito 13

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,16 +1,32 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Header from '../components/Header';
 
 class Feedback extends Component {
   render() {
+    const { assertions } = this.props;
+    const minAssertions = 3;
+
     return (
       <>
         <Header />
-        <h1 data-testid="feedback-text">Mensagem Feedback</h1>
+        <h1 data-testid="feedback-text">
+          {
+            assertions < minAssertions ? 'Could be better...' : 'Well Done!'
+          }
+        </h1>
       </>
     );
   }
 }
 
-export default connect()(Feedback);
+const mapStateToProps = (state) => ({
+  assertions: state.player.assertions,
+});
+
+Feedback.propTypes = {
+  assertions: PropTypes.number,
+}.isRequired;
+
+export default connect(mapStateToProps)(Feedback);
