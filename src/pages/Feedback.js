95c9f67a6696c3b0a78922b109be743d1e7073c6
Lalá Nametala <lalanametala@gmail.com>
2022-04-08 19:24:06 -0300

    [STYLE] Ranking - incompleto

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,7 +1,14 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { BsFillCheckCircleFill } from 'react-icons/bs';
+import { GiPodium } from 'react-icons/gi';
+import { MdReplay } from 'react-icons/md';
+import { RiGamepadFill } from 'react-icons/ri';
 import { connect } from 'react-redux';
 import Header from '../components/Header';
+import celebration from '../helpers/celebration.gif';
+import sad from '../helpers/sad.gif';
+import './Feedback.css';
 
 class Feedback extends Component {
   handlePlayAgain = () => {
@@ -21,35 +28,48 @@ class Feedback extends Component {
     return (
       <>
         <Header />
-        <h1 data-testid="feedback-text">
-          {
-            assertions < minAssertions ? 'Could be better...' : 'Well Done!'
-          }
-        </h1>
-        <p>
-          Pontuação total:
-          {' '}
-          <span data-testid="feedback-total-score">{score}</span>
-        </p>
-        <p>
-          Número de acertos:
-          {' '}
-          <span data-testid="feedback-total-question">{assertions}</span>
-        </p>
-        <button
-          type="button"
-          data-testid="btn-play-again"
-          onClick={ this.handlePlayAgain }
-        >
-          Play Again
-        </button>
-        <button
-          type="button"
-          data-testid="btn-ranking"
-          onClick={ this.handleRanking }
-        >
-          Ranking
-        </button>
+        <div className="feedback-page">
+          <h1 data-testid="feedback-text">
+            {
+              assertions < minAssertions ? 'Could be better...' : 'Well Done!'
+            }
+          </h1>
+          {assertions < minAssertions
+            ? <img src={ sad } alt="sad minions gif" />
+            : <img src={ celebration } alt="happy minions gif" />}
+          <p className="total-score">
+            <RiGamepadFill className="total-score-icon" />
+            Total Score:
+            {' '}
+            <span data-testid="feedback-total-score">{score}</span>
+          </p>
+          <p className="total-assertions">
+            <BsFillCheckCircleFill className="total-assert-icon" />
+            Assertions:
+            {' '}
+            <span data-testid="feedback-total-question">{assertions}</span>
+          </p>
+          <div className="feedback-btns">
+            <button
+              type="button"
+              data-testid="btn-play-again"
+              onClick={ this.handlePlayAgain }
+              className="btn-play-again"
+            >
+              <MdReplay className="replay-icon" />
+              Play Again
+            </button>
+            <button
+              type="button"
+              data-testid="btn-ranking"
+              onClick={ this.handleRanking }
+              className="btn-ranking"
+            >
+              <GiPodium />
+              Ranking
+            </button>
+          </div>
+        </div>
       </>
     );
   }
