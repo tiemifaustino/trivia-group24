aefadd7eebc2c55e344d07c5e8b3ddde612e9f9a
Lalá Nametala <lalanametala@gmail.com>
2022-04-08 23:43:25 -0300

    [STYLE] Componente Ranking.js

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,6 +1,8 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { GiPodium } from 'react-icons/gi';
 import { MdReplay } from 'react-icons/md';
+import logoTrivia from '../helpers/logoTrivia.png';
 import './Ranking.css';
 
 class Ranking extends Component {
@@ -25,39 +27,58 @@ class Ranking extends Component {
     ranking.sort(this.sorter);
 
     return (
-      <div className="ranking-page">
-        <h1
-          data-testid="ranking-title"
-          className="ranking-title"
-        >
-          Ranking
-        </h1>
-        <div className="ranking-table">
-          {ranking.map((player, index) => (
-            <div className="ranked-player" key={ index }>
-              <img
-                alt="icone gravatar"
-                src={ player.picture }
-              />
-              <p data-testid={ `player-name-${index}` }>
-                {player.name}
-              </p>
-              <p data-testid={ `player-score-${index}` }>
-                {player.score}
-              </p>
+      <>
+        <header className="header-game">
+          <div className="game-title">
+            <img src={ logoTrivia } alt="Imagem jogo" width="50px" />
+            <h3>TrybeTrivia</h3>
+          </div>
+          <div className="game-title">
+            <h3
+              data-testid="ranking-title"
+              className="ranking-title"
+            >
+              Ranking
+              {'  '}
+              <GiPodium className="podium-icon" />
+            </h3>
+          </div>
+        </header>
+        <div className="ranking-page">
+          <div className="ranking-table">
+            <div className="table-header">
+              <p>Player</p>
+              <p>Score</p>
             </div>
-          ))}
+            <hr />
+            {ranking.map((player, index) => (
+              <div className="ranked-player" key={ index }>
+                <div>
+                  <img
+                    alt="icone gravatar"
+                    src={ player.picture }
+                  />
+                  <p data-testid={ `player-name-${index}` }>
+                    {player.name}
+                  </p>
+                </div>
+                <p data-testid={ `player-score-${index}` }>
+                  {player.score}
+                </p>
+              </div>
+            ))}
+          </div>
+          <button
+            type="button"
+            data-testid="btn-go-home"
+            onClick={ this.handlePlayAgain }
+            className="btn-play-again ranking-btn"
+          >
+            <MdReplay className="replay-icon" />
+            Play Again
+          </button>
         </div>
-        <button
-          type="button"
-          data-testid="btn-go-home"
-          onClick={ this.handlePlayAgain }
-          className="btn-play-again ranking-btn"
-        >
-          <MdReplay className="replay-icon" />
-          Play Again
-        </button>
-      </div>
+      </>
     );
   }
 }
